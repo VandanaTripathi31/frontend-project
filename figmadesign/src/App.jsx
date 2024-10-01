0846917bc589af3723edb05b6f38fa8f290c5583
VandanaTripathi31 <tripathivandana086@gmail.com>
2024-10-01 15:46:46 +0530

    issue with navbar

--- figmadesign/src/App.jsx
+++ figmadesign/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import MyForm from './component/MyForm';
+import Navbar from './component/Navbar/Navbar';
 
 
 const App = () => {
   return (
     <div>
-      <h1>React Form with Vite</h1>
-      <MyForm/>
+      <Navbar/>
     </div>
   );
 };
---
+++ figmadesign/src/component/Navbar/Navbar.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        {/* Add logo here if needed */}
+      </div>
+      <ul className="app__navbar-links">
+        <li className="p__opensans"><a href="#home">Home</a></li>
+        <li className="p__opensans"><a href="#about">About</a></li>
+        <li className="p__opensans"><a href="#menu">Menu</a></li>
+        <li className="p__opensans"><a href="#awards">Awards</a></li>
+        <li className="p__opensans"><a href="#contact">Contact</a></li>
+      </ul>
+      <div className="app__navbar-login">
+        <a href="#login" className="p__opensans">Log In / Register</a>
+        <div></div>
+        <a href="/" className="p__opensans">Book Table</a>
+      </div>
+      <div className="app__navbar-smallscreen">
+        {/* Removed the icons */}
+        {/* Hamburger Menu button would go here */}
+
+        {toggleMenu && (
+          <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+            {/* Close button would go here */}
+            <ul className="app__navbar-smallscreen_links">
+              <li className="p__opensans"><a href="#home">Home</a></li>
+              <li className="p__opensans"><a href="#about">About</a></li>
+              <li className="p__opensans"><a href="#menu">Menu</a></li>
+              <li className="p__opensans"><a href="#awards">Awards</a></li>
+              <li className="p__opensans"><a href="#contact">Contact</a></li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
